c561dc94228c0fd608a7a2ee3d39ac1a7c9fcbdd
Carlos Filoteo <c.filoteo12@gmail.com>
2021-03-24 07:54:07 -0600

    Initialize with SvelteKit

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<main />
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+</style>
