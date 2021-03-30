e9eb79b0a60b2616019c6e23e774ab00667d3a2b
Carlos Filoteo <c.filoteo12@gmail.com>
2021-03-29 20:45:05 -0600

    Add required props for Article

--- src/lib/Article.svelte
+++ src/lib/Article.svelte
@@ -1,3 +1,9 @@
+<script>
+  import { requires } from '$lib/helper-utils';
+  requires($$props['aria-posinset'], 'Article: aria-posinset is required!');
+  requires($$props['aria-setsize'], 'Article: aria-setsize is required!');
+</script>
+
 <div role="article" tabindex="0" {...$$props}>
   <slot />
 </div>
