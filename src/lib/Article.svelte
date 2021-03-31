20d49ad6d5d19fc8cc6ce747efd265c26e924ea0
Carlos Filoteo <c.filoteo12@gmail.com>
2021-03-31 16:13:32 -0600

    Warn if aria-posinset is not a positive integer

--- src/lib/Article.svelte
+++ src/lib/Article.svelte
@@ -1,7 +1,12 @@
 <script>
-  import { requires } from '$lib/helper-utils';
-  requires($$props['aria-posinset'], 'Article: aria-posinset is required!');
-  requires($$props['aria-setsize'], 'Article: aria-setsize is required!');
+  import { requires } from '$lib/helper-utils'
+  requires($$props['aria-posinset'], 'Article: aria-posinset is required!')
+  requires($$props['aria-setsize'], 'Article: aria-setsize is required!')
+
+  if ($$props['aria-posinset'] < 1)
+    console.warn(
+      'aria-posinset should be a positive integer value that indicates the position of the article in the feed.'
+    )
 </script>
 
 <div role="article" tabindex="0" {...$$props}>
