7d946f0c4a677390294738e82a86ca30e523637c
Carlos Filoteo <c.filoteo12@gmail.com>
2021-03-31 16:12:13 -0600

    Formatting

--- src/lib/focus-utils.js
+++ src/lib/focus-utils.js
@@ -1,47 +1,50 @@
+const FOCUSABLES_SELECTOR =
+  'a, button, input, textarea, select, details, [tabindex]'
+
 export function isFocusable(element) {
-  if (element.tabIndex < 0) return false;
-  if (element.disabled) return false;
+  if (!element || element.tabIndex < 0 || element.disabled) return false
 
   switch (element.nodeName) {
     case 'A':
-      return !!element.href && element.rel != 'ignore';
+      return !!element.href && element.rel != 'ignore'
     case 'INPUT':
-      return element.type != 'hidden';
+      return element.type != 'hidden'
     case 'BUTTON':
     case 'SELECT':
+    case 'DETAILS':
     case 'TEXTAREA':
-      return true;
+      return true
     default:
-      return false;
+      return false
   }
-};
+}
 
-export function attemptFocus (element) {
-  if (!isFocusable(element)) return false;
+export function attemptFocus(element) {
+  if (!isFocusable(element)) return false
 
   try {
-    element.focus();
+    element.focus()
   } catch (e) {}
 
-  return document.activeElement === element;
-};
+  return document.activeElement === element
+}
 
-export function focusFirstDescendant (element) {
+export function focusFirstDescendant(element) {
   for (var i = 0; i < element.childNodes.length; i++) {
-    var child = element.childNodes[i];
+    var child = element.childNodes[i]
     if (attemptFocus(child) || focusFirstDescendant(child)) {
-      return true;
+      return true
     }
   }
-  return false;
-};
+  return false
+}
 
-export function focusLastDescendant (element) {
+export function focusLastDescendant(element) {
   for (var i = element.childNodes.length - 1; i >= 0; i--) {
-    var child = element.childNodes[i];
+    var child = element.childNodes[i]
     if (attemptFocus(child) || focusLastDescendant(child)) {
-      return true;
+      return true
     }
   }
-  return false;
-};
+  return false
+}
