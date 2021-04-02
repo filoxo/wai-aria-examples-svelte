f90e49eb15c773fc3d13d148e7f2ac27ea6efacc
Carlos Filoteo <c.filoteo12@gmail.com>
2021-04-02 14:48:46 -0600

    Refactor in order to handle only Enter or Space in addition to both; formatting

--- src/lib/keyboard-utils.js
+++ src/lib/keyboard-utils.js
@@ -1,17 +1,33 @@
 export const KeyCode = {
-  ENTER: "Enter",
-  SPACE: " ",
-  PAGE_UP: "PageUp",
-  PAGE_DOWN: "PageDown",
+  ENTER: 'Enter',
+  SPACE: ' ',
+  PAGE_UP: 'PageUp',
+  PAGE_DOWN: 'PageDown',
   HOME: 'Home',
-  END: 'End'
+  END: 'End',
 }
 
 export const handleKeyboardClick = (fn) => {
   return (e) => {
     switch (e.key) {
-      case KeyCode.SPACE: e.preventDefault(); // to prevent page scrolling on Space, and fall through
-      case KeyCode.ENTER: return fn(e);
+      case KeyCode.SPACE:
+        return handleSpaceAsClick(fn)(e)
+      case KeyCode.ENTER:
+        return handleEnterAsClick(fn)(e)
     }
+  }
+}
+
+export const handleEnterAsClick = (fn) => (e) => {
+  if (e.key === KeyCode.ENTER) {
+    return fn(e)
+  }
+}
+
+export const handleSpaceAsClick = (fn) => (e) => {
+  if (e.key === KeyCode.SPACE) {
+    // to prevent page scrolling on Space
+    e.preventDefault()
+    return fn(e)
   }
 }
