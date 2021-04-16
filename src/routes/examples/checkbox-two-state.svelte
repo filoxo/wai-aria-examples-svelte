87527e9935900781f8b91b23bef6bb6121a6c472
Carlos Filoteo <c.filoteo12@gmail.com>
2021-04-16 08:18:05 -0600

    Add lesson learned

--- src/routes/examples/checkbox-two-state.svelte
+++ src/routes/examples/checkbox-two-state.svelte
@@ -1,5 +1,5 @@
 <script>
-  import CheckboxTwoState from '../../lib/CheckboxTwoState.svelte';
+  import CheckboxTwoState from '../../lib/CheckboxTwoState.svelte'
 </script>
 
 <h3 id="id-group-label">Sandwich Condiments</h3>
@@ -33,6 +33,16 @@
     <li>
       Original example adds a <code>focus</code> class to the checkbox but it is
       unused from what I could tell; so I have chosen to omit it.
+    </li>
+    <li>
+      <code>aria-controls</code> feels like it makes it <em>harder</em> to get
+      it right, since relying on the DOM to be able to find/derive state is more
+      work than simply passing the state down through a framework. I decided to
+      bypass this by using
+      <a href="https://svelte.dev/docs#bind_element"
+        >Svelte <code>bind:this</code></a
+      > to give the Checkbox a reference to the rendered DOM node. This gives me
+      access to the element's ID.
     </li>
   </ul>
 </section>
