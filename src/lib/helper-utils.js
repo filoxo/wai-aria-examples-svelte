63627677a60869ef62a04b4f871affdacbaed9ff
Carlos Filoteo <c.filoteo12@gmail.com>
2021-05-19 21:08:23 -0600

    Add array index helpers

--- src/lib/helper-utils.js
+++ src/lib/helper-utils.js
@@ -1,3 +1,15 @@
 export function requires(value, errorMsg) {
-  if(value === undefined || value === null) throw new Error(errorMsg)
+  if (value === undefined || value === null) throw new Error(errorMsg)
+}
+
+export function nextIndex(current, arrayLength) {
+  const next = current + 1
+  const wrap = next % arrayLength
+  return wrap
+}
+
+export function prevIndex(current, arrayLength) {
+  const prev = current - 1
+  const wrap = prev < 0 ? arrayLength - 1 : prev
+  return wrap
 }
