bdd3f7e1f6b5be1bd32940e63a741ff24436f7c6
Carlos Filoteo <c.filoteo12@gmail.com>
2021-05-19 21:08:33 -0600

    Additional keycodes

--- src/lib/keyboard-utils.js
+++ src/lib/keyboard-utils.js
@@ -1,10 +1,15 @@
 export const KeyCode = Object.freeze({
   ENTER: 'Enter',
   SPACE: ' ',
+  ESC: 'Escape',
   PAGE_UP: 'PageUp',
   PAGE_DOWN: 'PageDown',
   HOME: 'Home',
   END: 'End',
+  UP: 'ArrowUp',
+  DOWN: 'ArrowDown',
+  RIGHT: 'ArrowRight',
+  LEFT: 'ArrowLeft',
 })
 
 export const handleKeyboardClick = (fn) => {
