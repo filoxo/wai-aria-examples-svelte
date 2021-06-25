6842a956bf625638a994f591ad0c945866890100
Carlos Filoteo <c.filoteo12@gmail.com>
2021-06-25 14:53:44 -0600

    Update to latest svelte kit with TypeScript enabled

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+import preprocess from 'svelte-preprocess';
+import netlify from '@sveltejs/adapter-netlify'
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+	preprocess: preprocess({ }),
+	kit: {
+		adapter: netlify(),
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
